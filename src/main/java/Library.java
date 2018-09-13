7539a6968ade887005cf0881f274af5ad6accbad
haoren <haoren@10.89.81.31>
2018-09-13 16:54:47 +0800

    Make a change to Library.main

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,8 +4,10 @@
 public class Library {
 	
 	public static void main(String args[]) {
-		System.out.println("Where there is a fire, commit and push.");
-		
+		Library lib = new Library();
+		if (lib.someLibraryMethod()) {
+			System.out.println("Where there is a fire, commit and push.");
+		}
 	}
 	
     public boolean someLibraryMethod() {
